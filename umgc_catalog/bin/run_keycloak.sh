d6f7a555bc0739eaf1695eaf2111500a7adb35c7
David Whiting <david.g.whiting@gmail.com>
2024-05-15 11:26:34 -0500

    removed umgc_catalog

--- umgc_catalog/bin/run_keycloak.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-## See https://wave.h2o.ai/docs/development
-docker run \
-  -p 8080:8080 \
-  -e KEYCLOAK_ADMIN=admin \
-  -e KEYCLOAK_ADMIN_PASSWORD=admin \
-  --volume ~/.keycloak:/opt/jboss/keycloak/standalone/data \
-  quay.io/keycloak/keycloak:21.0.1 \
-  start-dev
