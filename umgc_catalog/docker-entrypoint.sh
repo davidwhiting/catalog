fce2552e4f0963854e0b7fd02c8af086960bc411
David Whiting <david.g.whiting@gmail.com>
2024-06-13 14:53:49 -0500

    updated umgc_catalog contents

--- umgc_catalog/docker-entrypoint.sh
+++ umgc_catalog/docker-entrypoint.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
 
-export VENV="${HOME}/venv"
-
 set -e
 export H2O_WAVE_ADDRESS="http://127.0.0.1:${H2O_WAVE_PORT}"
-printf '\n$ ( cd %s && ./waved -listen ":%s" & )\n\n' "${WAVE_PATH}" "${H2O_WAVE_PORT}"
-(cd "${WAVE_PATH}" && ./waved -listen ":${H2O_WAVE_PORT}" &)
+printf '\n$ ( cd %s && ./waved -listen ":%s" & )\n\n' "${H2O_WAVE_PATH}" "${H2O_WAVE_PORT}"
+(cd "${H2O_WAVE_PATH}" && ./waved -listen ":${H2O_WAVE_PORT}" &)
 sleep 3
 
 printf '\n$ $VENV/bin/wave run --no-reload --no-autostart %s\n\n' "$PYTHON_MODULE"
-exec $VENV/bin/wave run --no-reload --no-autostart "$PYTHON_MODULE"
-
-#source $VENV/bin/activate
-#exec wave run --no-reload --no-autostart "$PYTHON_MODULE"
+exec $HOME/.venv/bin/wave run --no-reload --no-autostart "$PYTHON_MODULE"
