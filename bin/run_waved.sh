b40ec8fae0f80c65fc85262921a10bb790dca358
David Whiting <aspencoveconsulting@gmail.com>
2024-06-20 09:28:54 -0500

    reorganized Catalog

--- bin/run_waved.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd venv && ./waved \
-    -oidc-client-id wave \
-    -oidc-client-secret aJ0wyZbXiBdGxXbKxAZu73Dm3Wu2wMb2 \
-    -oidc-redirect-url http://localhost:10101/_auth/callback \
-    -oidc-provider-url http://localhost:8080/realms/master \
-    -oidc-end-session-url http://localhost:8080/realms/master/protocol/openid-connect/logout
